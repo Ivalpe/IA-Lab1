15431a1ae42e256a17afe103293767f6e5186561
Ivalpe <ivanalvarezperea@gmail.com>
2025-10-24 13:54:28 +0200

    Animal Crossing colab with GTA6

---
+++ Assets/Scripts/Villager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Villager : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+        
+    }
+}
