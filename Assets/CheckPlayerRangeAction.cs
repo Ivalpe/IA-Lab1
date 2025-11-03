123a6c19de134808b4b629a1bd982dd1f1f9c58c
Ivalpe <ivanalvarezperea@gmail.com>
2025-11-03 12:11:04 +0100

    Trying things ❤️✨

--- Assets/CheckPlayerRangeAction.cs
+++ Assets/CheckPlayerRangeAction.cs
@@ -3,6 +3,7 @@ using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using System.Collections;
 
 [Serializable, GeneratePropertyBag]
 [NodeDescription(name: "CheckPlayerRange", story: "Check [Target] in [radius] and [angle] for [pos]", category: "Action", id: "6bab6e8a98ee6d3c6ed8fd5bb18036a8")]
@@ -25,6 +26,57 @@ public partial class CheckPlayerRangeAction : Action
 
     protected override void OnEnd()
     {
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+                    canSeePlayer = true;
+                    //PosTarget.Value = target.position;
+                    foreach (var bbv in m_blackboardAsset.Blackboard.Variables)
+                    {
+                        Debug.Log("We are searching");
+                        if (bbv.Name == "PosTarget")
+                        {
+                            if (bbv is BlackboardVariable<Vector3> vectorBbv)
+                            {
+                                Debug.Log("Found");
+                                vectorBbv.Value = target.position;
+                            }
+                        }
+                    }
+                }
+                else
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+        }
+        else if (canSeePlayer)
+            canSeePlayer = false;
     }
 }
 
